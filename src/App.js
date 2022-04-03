ac5dc7a11fcf483e37a3252411efa52e2b4ad6be
bm <victory289@naver.com>
2022-04-03 21:10:39 +0900

    JSX의 기본 문법변환 내용 설명

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Expenses from './components/Expenses';
 
 function App() {
@@ -29,7 +30,21 @@ function App() {
   ];
 
 
+  // 순수 REACT 문법
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', null, "Let's get started~~!!!!"),
+  //   React.createElement(Expenses, {items: expenses}),
+  // )
 
+  /// 반환할 때, 오직 1개의 wrapper 가 있어야 하는 이유는.
+  // return에서 반환할 수 있는 Element가 오직 1개만 가능하기 떄문이다.
+  // React.createElement(...) <---- String이 아니라, 엘리먼트기 때문에 2개 이상 반환이 불가능하다는거.
+
+
+
+  // JSX 문법
   return (
     <main>
       <h2>Let's get started!!!!</h2>
