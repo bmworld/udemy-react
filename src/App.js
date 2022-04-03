a30f3b787bf26eea4d138e81468e1cd02080a6aa
bm <victory289@naver.com>
2022-04-03 19:26:28 +0900

    -37강까지

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import ExpenseItem from './components/ExpenseItem.jsx'
+import ExpenseItem from './components/ExpenseItem.jsx';
 
 function App() {
+  
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2021, 2, 12)
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+
+  let List_ExpenseItem = [];
+  
+  for(let i =0; i < expenses.length; i++){
+    List_ExpenseItem.push(<ExpenseItem
+      key= {i} 
+      title={expenses[i].title}
+      amount={expenses[i].amount}
+      date={expenses[i].date}
+    ></ExpenseItem>)
+  }
+
+
 
   return (
     <div>
-      <h2>Let's get started!!</h2>
-      <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
+      <h2>Let's get started!!!!</h2>
+      {List_ExpenseItem}
     </div>
   );
 }
 
 export default App;
+ 
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,19 +1,17 @@
 import React from "react";
+import ExpenseDate from './ExpenseDate';
 import "./ExpenseItem.css";
 
 // 중괄호 사이에는 기본 자바스크립트 표현을 사용할 수 있다.
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = "297.67";
+function ExpenseItem(props) {
+
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toDateString()}</div>
-      <div>{expenseDate.toISOString()}</div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
